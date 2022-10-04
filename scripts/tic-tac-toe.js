fd1bc3067f1fe0f9b1564366944ca2152f4f37d6
levib00 <levib8@hotmail.com>
2022-10-03 23:55:00 -0400

    attach player selection logic to buttons

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -20,35 +20,62 @@ const game = (() => {
         playButton.forEach((playButtons) => {
             playButtons.addEventListener('click', test)
             playButtons.innerHTML = gameBoard.myArray[i];
-            i++
-        } 
-        )
+            i++;
+        })
     });
     render()
-    const human1 = true;
-    const human2 = true;
-    const ai1 = false;
-    const ai2 = false;
+
+    const human1 = document.getElementById("human1");
+    const human2 = document.getElementById("human2");
+    const cpu1 = document.getElementById("cpu1");
+    const cpu2 = document.getElementById("cpu2")
+
+    human1.addEventListener("click",() => {
+        players.human1 = true;
+        players.ai1 = false;
+        createPlayer()
+    })
+
+    human2.addEventListener("click",() => {
+        players.human2 = true;
+        players.ai2 = false;
+        createPlayer()
+    })
+
+    cpu1.addEventListener("click",() => {
+        players.ai1 = true;
+        players.human1 = false;
+        createPlayer()
+    })
+
+    cpu2.addEventListener("click",() => {
+        players.ai2 = true;
+        players.human2 = false;
+        createPlayer()
+    })
+
+    const players = {
+        human1: false,
+        human2: false,
+        ai1: true,
+        ai2: true
+    }
     const createPlayer = () => {
         const getName = () =>  {
-            if ((!human1 && !ai1) || (!human2 && !ai2)) {
-                console.log({human1, human2, ai1, ai2},"condition: return")
-            } else if ((!human1 && ai1) && (!human2 && ai2)) {
-                console.log({human1, human2, ai1, ai2}, "condition: both ai")
-            } else if ((human1 && !ai1) && (human2 && !ai2)) {
-                console.log({human1, human2, ai1, ai2}, "condition: both human")
-            } else if ((human1 ^ human2) && (ai1 ^ ai2)) {
-                console.log({human1, human2, ai1, ai2}, "condition: human vs. ai")
-            } else if ((human1 && human2) || (ai1 && ai2)) {
-                console.log({human1, human2, ai1, ai2}, "condition: error")
+            if ((!players.human1 && !players.ai1) || (!players.human2 && !players.ai2)) {
+                console.log({players},"condition: return")
+            } else if ((!players.human1 && players.ai1) && (!players.human2 && players.ai2)) {
+                console.log({players}, "condition: both ai")
+            } else if ((players.human1 && !players.ai1) && (players.human2 && !players.ai2)) {
+                console.log({players}, "condition: both human")
+            } else if ((players.human1 ^ players.human2) && (players.ai1 ^ players.ai2)) {
+                console.log({players}, "condition: human vs. ai")
+            } else if ((players.human1 && players.human2) || (players.ai1 && players.ai2)) {
+                console.log({players}, "condition: error")
             }
-
         }
         getName()
     }
     createPlayer()
-    const players = {
-        
-    }
     
 })();

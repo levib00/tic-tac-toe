c34480118d6fc8f40cc0daca8f35640e9475eb6a
levib00 <levib8@hotmail.com>
2022-10-04 22:20:34 -0400

    Alternate x and o when placing on board

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -2,33 +2,44 @@ const game = (() => {
     const playButton = document.querySelectorAll(".play-button")
 
     const gameBoard = {
-        myArray : [1,2,3,4,5,6,7,8,9],
+        boardArray : [1,2,3,4,5,6,7,8,9],
     };
 
-    function test() {
+    const turns = {
+        xTurn : true,
+        oTurn : false
+    }
+
+    function addToBoardArray() {
         if (this.innerHTML === "X" || this.innerHTML === "O") {
             console.log(this.innerHTML)
             return
-        } else {
-        gameBoard.myArray[Array.prototype.indexOf.call(playButton, this)] = "X";
-        render()
+        } else if (turns.xTurn) {
+        gameBoard.boardArray[Array.prototype.indexOf.call(playButton, this)] = "X";
+        turns.xTurn = false
+        turns.oTurn = true
+        } else if (turns.oTurn) {
+            gameBoard.boardArray[Array.prototype.indexOf.call(playButton, this)] = "O";
+            turns.oTurn = false
+            turns.xTurn = true
         }
+        render()
     }
-
-    const render = (function() {
-        i = 0
-        playButton.forEach((playButtons) => {
-            playButtons.addEventListener('click', test)
-            playButtons.innerHTML = gameBoard.myArray[i];
-            i++;
-        })
-    });
-    render()
 
     const human1 = document.getElementById("human1");
     const human2 = document.getElementById("human2");
     const cpu1 = document.getElementById("cpu1");
     const cpu2 = document.getElementById("cpu2")
+
+    const render = (function() {
+        i = 0
+        playButton.forEach((playButtons) => {
+            playButtons.addEventListener('click', addToBoardArray)
+            playButtons.innerHTML = gameBoard.boardArray[i];
+            i++;
+        })
+    });
+    render()
 
     human1.addEventListener("click",() => {
         players.human1 = true;
@@ -57,11 +68,12 @@ const game = (() => {
     const players = {
         human1: false,
         human2: false,
-        ai1: true,
-        ai2: true
+        ai1: false,
+        ai2: false
     }
+
     const createPlayer = () => {
-        const getName = () =>  {
+        const getName = (() =>  {
             if ((!players.human1 && !players.ai1) || (!players.human2 && !players.ai2)) {
                 console.log({players},"condition: return")
             } else if ((!players.human1 && players.ai1) && (!players.human2 && players.ai2)) {
@@ -73,9 +85,6 @@ const game = (() => {
             } else if ((players.human1 && players.human2) || (players.ai1 && players.ai2)) {
                 console.log({players}, "condition: error")
             }
-        }
-        getName()
+        })()
     }
-    createPlayer()
-    
 })();
